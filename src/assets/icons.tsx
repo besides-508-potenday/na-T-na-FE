127c1c0fb6b1f827cbe21d824d3b95812173beaf
Theo Jin <jhb_august@naver.com>
2025-08-03 04:39:02 +0900

    FEAT:set icons

---
+++ src/assets/icons.tsx
@@ -0,0 +1,83 @@
+export const StepIcon = ({ size = 24, fill = '#18181B' }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14 20H16V18H14V20ZM4 16H16V18H18V16H20V14H18V12H16V14H6V4H4V16ZM14 12H16V10H14V12Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
+
+export const XIcon = ({ size = 24, fill = '#18181B' }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 14H0V12H2V14ZM14 14H12V12H14V14ZM4 10V12H2V10H4ZM12 12H10V10H12V12ZM6 10H4V8H6V10ZM10 10H8V8H10V10ZM8 8H6V6H8V8ZM6 6H4V4H6V6ZM10 6H8V4H10V6ZM4 4H2V2H4V4ZM12 4H10V2H12V4ZM2 2H0V0H2V2ZM14 2H12V0H14V2Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
+
+export const SquareIcon = ({ size = 24, fill = '#18181B' }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 18 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M2 18H0V16H2V18ZM6 18H4V16H6V18ZM10 18H8V16H10V18ZM14 18H12V16H14V18ZM18 18H16V16H18V18ZM2 14H0V12H2V14ZM18 14H16V12H18V14ZM2 10H0V8H2V10ZM18 10H16V8H18V10ZM2 6H0V4H2V6ZM18 6H16V4H18V6ZM2 2H0V0H2V2ZM6 2H4V0H6V2ZM10 2H8V0H10V2ZM14 2H12V0H14V2ZM18 2H16V0H18V2Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
+
+export const ChevronRightIcon = ({ size = 24, fill = '#18181B' }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 8 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M8 8L8 6L6 6L6 8L8 8ZM6 6L6 4L4 4L4 6L6 6ZM6 10L6 8L4 8L4 10L6 10ZM4 4L4 2L2 2L2 4L4 4ZM4 12L4 10L2 10L2 12L4 12ZM2 2L2 -8.74228e-08L0 0L8.74232e-08 2L2 2ZM2 14L2 12L5.24537e-07 12L6.11959e-07 14L2 14Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
+export const ChevronLeftIcon = ({ size = 24, fill = '#18181B' }) => {
+  return (
+    <svg
+      width={size}
+      height={size}
+      viewBox="0 0 8 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M-3.49691e-07 8L-2.62268e-07 6L2 6L2 8L-3.49691e-07 8ZM2 6L2 4L4 4L4 6L2 6ZM2 10L2 8L4 8L4 10L2 10ZM4 4L4 2L6 2L6 4L4 4ZM4 12L4 10L6 10L6 12L4 12ZM6 2L6 -8.74228e-08L8 0L8 2L6 2ZM6 14L6 12L8 12L8 14L6 14Z"
+        fill={fill}
+      />
+    </svg>
+  );
+};
--- src/modules/home/ui/components/IntroCard.tsx
+++ src/modules/home/ui/components/IntroCard.tsx
@@ -1,4 +1,10 @@
-import { ChevronLeft, ChevronRight, Minus, Square, X } from 'lucide-react';
+import { Minus } from 'lucide-react';
+import {
+  XIcon,
+  SquareIcon,
+  ChevronRightIcon,
+  ChevronLeftIcon,
+} from '@/assets/icons';
 
 export default function IntroCard() {
   return (
@@ -7,13 +13,13 @@ export default function IntroCard() {
       <div className="w-full bg-[#C6CAFE] border-b-2 border-[#7538C5] rounded-t-[30px] px-4 py-3 flex items-center gap-2">
         {/* 좌측 네비게이션 */}
         <div className="flex items-center gap-1">
-          <ChevronLeft className="w-6 h-6 text-black" />
-          <ChevronRight className="w-6 h-6 text-black" />
+          <ChevronLeftIcon size={18} fill="#18181B" />
+          <ChevronRightIcon size={18} fill="#18181B" />
         </div>
 
         {/* 중앙 주소창/상태 */}
         <div className="flex-1 flex justify-center">
-          <div className="bg-white/50 border-[1.5px] border-[#9E85FD] rounded px-2 py-1">
+          <div className="bg-white/50 border-[1.5px] border-[#9E85FD] rounded px-13 py-1">
             <span className="text-[#18181B] text-lg font-semibold">
               시작중...
             </span>
@@ -23,8 +29,8 @@ export default function IntroCard() {
         {/* 우측 윈도우 컨트롤 */}
         <div className="flex items-center gap-[7px]">
           <Minus className="w-6 h-6 text-black" />
-          <Square className="w-6 h-6 text-black" />
-          <X className="w-6 h-6 text-black" />
+          <SquareIcon size={18} fill="#18181B" />
+          <XIcon size={18} fill="#18181B" />
         </div>
       </div>
 
